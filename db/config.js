d0e26035080dd94946a6be0f98fb80bfb3c0ee2e
Hack Reactor Students <accounts@hackreactor.com>
2015-11-12 12:30:03 -0800

    Test examples connected. Now writing creating tests

--- db/config.js
+++ db/config.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 
 // Change for deployment
 //process.env.MONGOLAB_URI || 'mongodb://127.0.0.1'
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect('mongodb://127.0.0.1');
 
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'eonnection error:'));
 db.once('open', function(cb) {
-  console.log('connected');
+  console.log('connected to db');
 });
 
 module.exports = db;
