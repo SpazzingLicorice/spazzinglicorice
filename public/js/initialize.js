96def16ef4953d254d10016e584a5eb123c59473
Michelle Lee <michelle.mh.lee92@gmail.com>
2015-11-16 16:19:19 -0800

    (bugfix) Add isAnotherUserActive loops to fix bugs

--- public/js/initialize.js
+++ public/js/initialize.js
@@ -29,50 +29,57 @@ $(function() {
 
       // Add the first mouse coordinates to stroke array for storage
       App.stroke.push([App.mouse.x, App.mouse.Y]);
+    } else {
+      console.log('Another user is drawing - please wait.');
     }
-
-
   });
 
 
   // Mousedrag event
   App.canvas.on('drag', function(e) {
-    if (App.mouse.click) {
-      console.log("Dragging");
-      App.mouse.drag = true;
+    if (!App.isAnotherUserActive) {
+      if (App.mouse.click) {
+        console.log("Dragging");
+        App.mouse.drag = true;
 
-      // Find x,y coordinates of dragging
-      var x = e.offsetX;
-      var y = e.offsetY;
+        // Find x,y coordinates of dragging
+        var x = e.offsetX;
+        var y = e.offsetY;
 
-      // Render the drawing
-      App.draw(x, y);
+        // Render the drawing
+        App.draw(x, y);
 
-      console.log("Currently drawing coordinates", [x, y]);
+        console.log("Currently drawing coordinates", [x, y]);
 
-      // Continue to push coordinates to stroke array (as part of storage)
-      App.stroke.push([x, y]);
+        // Continue to push coordinates to stroke array (as part of storage)
+        App.stroke.push([x, y]);
 
-      // Emit x, y in array
-      App.socket.emit('drag', [x, y]);
+        // Emit x, y in array
+        App.socket.emit('drag', [x, y]);
+      }
+    } else {
+      console.log('Another user is drawing - please wait.');
     }
-
-
   });
 
   // Mouse dragend event
   App.canvas.on('dragend', function(e) {
-    App.mouse.drag = false;
-    App.mouse.click = false;
+    if (!App.isAnotherUserActive) {
+      App.mouse.drag = false;
+      App.mouse.click = false;
 
-    // Sample of resulting data to be pushed to db
-    console.log("Drawing is finished and its data is being pushed to the server", [App.stroke, App.pen]);
+      // Sample of resulting data to be pushed to db
+      console.log("Drawing is finished and its data is being pushed to the server", [App.stroke, App.pen]);
 
-    // Empty App.stroke
-    App.stroke = [];
+      // Empty App.stroke
+      App.stroke = [];
 
-    // Tell socket that we've finished sending data
-    App.socket.emit('end', null);
+      // Tell socket that we've finished sending data
+      App.socket.emit('end', null);
+
+    } else {
+      console.log('Another user is drawing - please wait.');
+    }
   });
 
   App.canvas.on('mouseleave', function(e) {
